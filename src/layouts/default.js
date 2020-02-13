23892f9b308b35315f73aa6d6db24bd676d5c0ec
Marco Mark <marco@silvanite.com>
2020-02-13 21:40:42 +0000

    Layout additions and components

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -1,18 +1,39 @@
 import React from "react"
+import tw from 'tailwind.macro'
 import PropTypes from "prop-types"
 import { Global } from "@emotion/core"
 import { Body } from '../styles/global'
 import Header from '../components/header'
 import Footer from '../components/footer'
 import Believe from '../components/believe'
+import Contact from '../components/contact'
+import ImageGrid from '../components/imagegrid'
 
 const Layout = ({ children }) => {
+  const Wrapper = tw.div`mb-20 flex flex-wrap font-body w-full`
+  const Container = tw.div`w-full max-w-5xl mx-auto flex flex-wrap px-8`
+  const Main = tw.div`w-full md:w-3/5`
+  const Side = tw.div`w-full md:w-2/5 flex flex-wrap p-2 content-start`
+  const Sticky = tw.div`sticky top-0`
+
   return (
     <>
       <Global styles={Body}></Global>
-      <Header />
-      <Believe />
-      {children}
+      <Wrapper>
+        <Header />
+        <Believe />
+        <Container>
+          <Main>
+            {children}
+          </Main>
+          <Side>
+            <Sticky>
+              <Contact />
+              <ImageGrid />
+            </Sticky>
+          </Side>
+        </Container>
+      </Wrapper>
       <Footer />
     </>
   )

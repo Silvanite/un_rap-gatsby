243deb2823226fe64ac67a6f7ef416ac38dfa122
Marco Mark <marco@silvanite.com>
2020-02-13 21:39:44 +0000

    Scope components

--- src/components/believe.js
+++ src/components/believe.js
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import tw from "tailwind.macro"
 
-const Wrapper = tw.div`my-8 px-8 py-16 text-3xl text-gray-700 leading-none font-display bg-orange-100 md:mt-0`
-const Container = tw.div`max-w-xl mx-auto md:px-8 md:text-center`
-const Message = tw.span`font-bold text-orange-500 block md:inline md:pl-2`
-
 const Believe = () => {
   const [index, setIndex] = useState(0)
+
+  const Wrapper = tw.div`my-8 px-8 py-16 text-3xl text-gray-700 leading-none font-display bg-orange-100 md:mt-0 w-full`
+  const Container = tw.div`max-w-xl mx-auto md:px-8 md:text-center`
+  const Message = tw.span`font-bold text-orange-500 block md:inline md:pl-2 w-full`
 
   const {
     prismic: {
--- src/components/footer.js
+++ src/components/footer.js
@@ -9,16 +9,16 @@ import Step1Img from "../images/step1.png"
 import Step2Img from "../images/step2.png"
 import Step3Img from "../images/step3.png"
 
-const Wrapper = tw.div`fixed bottom-0 left-0 w-full bg-white p-2 border-t border-gray-300`
-const Container = tw.div`flex justify-around max-w-2xl mx-auto`
-const MenuItem = tw.div`text-center`
-const HomeLink = styled(Link)`
-  ${tw`border-r-2 pr-8`}
-`
-const Icon = tw.img`inline-block h-16 w-16`
-const Logo = tw.img`hidden md:inline-block flex-1 h-12`
-
 const Footer = () => {
+  const Wrapper = tw.div`fixed bottom-0 left-0 w-full bg-white p-2 border-t border-gray-300`
+  const Container = tw.div`flex justify-around max-w-2xl mx-auto`
+  const MenuItem = tw.div`text-center`
+  const HomeLink = styled(Link)`
+    ${tw`border-r-2 pr-8`}
+  `
+  const Icon = tw.img`inline-block h-16 w-16`
+  const Logo = tw.img`hidden md:inline-block flex-1 h-12`
+
   return (
     <Wrapper>
       <Container>

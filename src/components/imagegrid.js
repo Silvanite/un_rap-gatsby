77f231844134d199b689b463723593ff00839234
Marco Mark <m2de@outlook.com>
2020-02-14 18:18:57 +0000

    Image grid

--- src/components/imagegrid.js
+++ src/components/imagegrid.js
@@ -1,24 +1,73 @@
-import React from 'react'
+import React from "react"
+import Img from 'gatsby-image'
+import tw from 'tailwind.macro'
+import styled from '@emotion/styled'
+import { useStaticQuery, graphql } from "gatsby"
 
 const ImageGrid = () => {
+  const images = useStaticQuery(graphql`
+    query {
+      image1: file(relativePath: { eq: "sidebar/hannah-un_rap.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 368) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      image2: file(relativePath: { eq: "sidebar/shop-window.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 368) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      image3: file(relativePath: { eq: "sidebar/sunflower-jars-13.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 368) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      image4: file(relativePath: { eq: "sidebar/staff-refill.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 368) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      image5: file(relativePath: { eq: "sidebar/customer-jar.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 368) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
 
-    return (
-        <div class="flex flex-wrap w-full">
-            <div class="p-2 w-1/2">
-                <img class="w-full zoomable" src="../../assets/hannah-un_rap.jpg" alt="" />
-            </div>
-            <div class="p-2 w-1/2">
-                <img class="w-full zoomable" src="../../assets/shop-window.jpg" alt="" />
-            </div>
-            <div class="p-2 w-1/2">
-                <img class="w-full zoomable" src="../../assets/sunflower-jars-13.jpg" alt="" />
-            </div>
-            <div class="p-2 w-1/2 flex flex-col justify-between">
-                <img class="w-full zoomable" src="../../assets/staff-refill.jpg" alt="" />
-                <img class="w-full zoomable" src="../../assets/customer-jar.jpg" alt="" />
-            </div>
-        </div>
-    )
+  const Wrapper = tw.div`flex flex-wrap w-full`
+  const ImageContainer = tw.div`p-2 w-1/2`
+  const DualImageContainer = styled(ImageContainer)`
+    ${tw`flex flex-col justify-between`}
+  `
+
+  return (
+    <Wrapper>
+      <ImageContainer>
+        <Img fluid={images.image1.childImageSharp.fluid} />
+      </ImageContainer>
+      <ImageContainer>
+        <Img fluid={images.image2.childImageSharp.fluid} />
+      </ImageContainer>
+      <ImageContainer>
+        <Img fluid={images.image3.childImageSharp.fluid} />
+      </ImageContainer>
+      <DualImageContainer>
+        <Img fluid={images.image4.childImageSharp.fluid} />
+        <Img fluid={images.image5.childImageSharp.fluid} />
+      </DualImageContainer>
+    </Wrapper>
+  )
 }
 
 export default ImageGrid
